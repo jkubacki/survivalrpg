776b212e6b54830921cabf34a35e881d5c515674
Jakub Kubacki <jakub.kubacki@netguru.co>
2015-04-05 20:26:11 +0200

    Add users to draw view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :encounters
   resources :places
   devise_for :users
-  root to: 'places#index'
+  root to: 'games#new'
   get 'games/new'
   post 'games/draw'
   get 'games/reward'
